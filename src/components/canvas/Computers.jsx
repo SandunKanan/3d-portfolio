d275086404974d5f0786c1c0e13eb3504da35634
Sandun Kanangama <sandunkanangama@gmail.com>
2024-05-08 00:59:03 +0900

    Computer component:

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -27,8 +27,14 @@ const ComputersCanvas = () => {
       gl={{ preserveDrawingBuffer: true }}
     >
       <Suspense fallback={<CanvasLoader />}>
-        <OrbitControls enableZoom={false} />
+        <OrbitControls
+          enableZoom={false}
+          maxPolarAngle={Math.PI /2}
+          minPolarAngle={Math.PI /2}
+        />
+        <Computers />
       </Suspense>
+      <Preload all />
     </Canvas>
   )
 }
